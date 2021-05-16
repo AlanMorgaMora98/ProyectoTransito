480c7bbb72835b150ed816458b8f626c9442e591
Emilio <enano_2206@hotmail.com>
2021-05-16 18:01:55 -0500

    POCOs
    Elaboración de los POCOs

---
+++ BaseDeDatos/POCOs/DelegacionesMunicipales.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseDeDatos.POCOs
+{
+    class DelegacionesMunicipales
+    {
+        private int idDelegacion;
+        private string nombre;
+        private int numero;
+        private string colonia;
+        private string calle;
+        private int codigoPostal;
+        private string correo;
+        private string telefono;
+        private string municipio;
+
+        public int IdDelegacion { get => idDelegacion; set => idDelegacion = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public int Numero { get => numero; set => numero = value; }
+        public string Colonia { get => colonia; set => colonia = value; }
+        public string Calle { get => calle; set => calle = value; }
+        public int CodigoPostal { get => codigoPostal; set => codigoPostal = value; }
+        public string Correo { get => correo; set => correo = value; }
+        public string Telefono { get => telefono; set => telefono = value; }
+        public string Municipio { get => municipio; set => municipio = value; }
+    }
+}
